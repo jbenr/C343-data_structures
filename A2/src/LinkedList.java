0899658214f218876a3b56fc63a1d429f733c189
Benjamin <benjamin@149-161-144-196.dhcp-bl.indiana.edu>
2020-01-29 11:31:59 -0500

    shtuff

--- A2/src/LinkedList.java
+++ A2/src/LinkedList.java
@@ -2,7 +2,7 @@
 
 class LinkedList<E> implements LinkedListI<E> {
     private List<E> elements;
-    private E elem;
+    private E node;
     private LinkedList<E> rest;
 
     LinkedList (E node, LinkedList<E> rest) {
@@ -15,27 +15,32 @@ class LinkedList<E> implements LinkedListI<E> {
 
     public int size() {
         int count = 0;
-        while (this.elements != null) {
-            count = count + 1;
+        if(this.node != null){
+            count++;
+            count = count + this.rest.size();
         }
         return count;
     }
 
     public void addFirst(E elem) {
+        node = elem;
+        rest = new LinkedList<E>(node, rest);
     }
 
     public void addLast(E elem) {
-
+        rest =
     }
 
     public E getFirst() throws NoSuchElementE {
         if (this.size() == 0)
             throw new NoSuchElementE();
-        return elem;
+        return node;
     }
 
     public E getLast() throws NoSuchElementE {
-        return null;
+        for(int i = 0; i < size(); i++){
+            
+        }
     }
 
     public E removeFirst() throws NoSuchElementE {
