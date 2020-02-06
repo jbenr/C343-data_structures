cfc80c1dd766bd441c827a6f7ebea0968c443e58
Benjamin <benjamin@149-161-150-75.dhcp-bl.indiana.edu>
2020-02-05 19:57:37 -0500

    finished DynamicArray

--- A3/src/DynamicArray.java
+++ A3/src/DynamicArray.java
@@ -34,53 +34,115 @@ public class DynamicArray<E> implements StackI<E>, QueueI<E>, DequeI<E> {
     }
 
     // Complete the definitions of the following methods from the interfaces
-
-    public int size () {
-	return 0;
+    public void clear () {
+        Arrays.fill(elements, Optional.empty());
+        front = capacity - 1;
+        back = 0;
+        size = 0;
     }
 
+    public int size () {
+	    return size;
+    }
+
+    // stack methods: delegate to dequeue
     public void push(E item) {
+        addFirst(item);
     }
 
     public E peek() throws NoSuchElementE {
-        return null;
+        return getFirst();
     }
 
     public E pop() throws NoSuchElementE {
-        return null;
+        return removeFirst();
     }
 
+    // queue methods: delegate to dequeue
     public void offer(E elem) {
+        addLast(elem);
     }
 
     public E poll() throws NoSuchElementE {
-        return null;
+        return getFirst();
     }
 
     public E remove() throws NoSuchElementE {
-        return null;
+        return removeFirst();
     }
 
+    // dequeue methods
     public void addFirst(E elem) {
+        if (size == capacity) doubleCapacity();
+        elements[front] = Optional.of(elem);
+        front = Math.floorMod(front - 1, capacity);
+        size++;
     }
 
     public void addLast(E elem) {
+        if (size == capacity) doubleCapacity();
+        elements[back] = Optional.of(elem);
+        back = Math.floorMod(back + 1, capacity);
+        size++;
     }
 
     public E getFirst() throws NoSuchElementE {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementE();
+        } else {
+            return elements[Math.floorMod(front + 1, capacity)].get();
+        }
     }
 
     public E getLast() throws NoSuchElementE {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementE();
+        } else {
+            return elements[Math.floorMod(back - 1, capacity)].get();
+        }
     }
 
     public E removeFirst() throws NoSuchElementE {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementE();
+        } else {
+            front = Math.floorMod(front + 1, capacity);
+            E apple = elements[front].get();
+            elements[front] = Optional.empty();
+            size--;
+            return apple;
+        }
     }
 
     public E removeLast() throws NoSuchElementE {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementE();
+        } else {
+            back = Math.floorMod(back -1, capacity);
+            E orange = elements[back].get();
+            elements[back] = Optional.empty();
+            size--;
+            return orange;
+        }
+    }
+
+    private void doubleCapacity () {
+        // newElements ...
+        // fill
+        // copy old elements to newElements
+        // loop from i=0; to capacity - 1
+        // newElements[i] = old elements front + 1 + i
+        // set back and front
+        // capacity = 2 capacity
+        Optional<E>[] newElements = (Optional<E>[]) Array.newInstance(Optional.class, 2*capacity);
+        Arrays.fill(newElements, Optional.empty());
+        for (int i = 0; i < capacity; i++) {
+            newElements[i] = elements[Math.floorMod(front + 1 + i,capacity)];
+        }
+        capacity = capacity*2;
+        front = capacity - 1;
+        back = capacity / 2;
+        elements = newElements;
     }
 
     // the following methods are used for debugging and testing;
