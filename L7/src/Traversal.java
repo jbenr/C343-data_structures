746fde020f2f8e8b561198648c15685a09da04a0
benreich <benreich@iu.edu>
2020-02-28 14:54:29 -0500

    lab7

--- L7/src/Traversal.java
+++ L7/src/Traversal.java
@@ -33,21 +33,60 @@ class Traversal<E> {
     
     // pre-order recursive
     static <E> List<E> preOrderRecursive(BinTree<E> tree){
-        return new Empty<>();
+        try {
+            Node<E> left = new Node<>(tree.getData(),preOrderRecursive(tree.getLeftT()));
+            return left.append(preOrderRecursive(tree.getRightT()));
+        } catch (EmptyTreeE e) {
+            return new Empty<>();
+        }
     }
 
     // pre-order iterative
     static <E> List<E> preOrderIterative(BinTree<E> tree){
-	return new Empty<>();
+        List<E> result = new Empty<>();
+        Stack<BinTree<E>> stack = new Stack<>();
+        VISIT_NODE: while (true) {
+            try {
+                stack.push(tree.getRightT());
+                result = result.append(new Node<>(tree.getData(), new Empty<>()));
+                tree = tree.getLeftT();
+            } catch (EmptyTreeE e) {
+                while (!stack.isEmpty()) {
+                    BinTree<E> frame = stack.pop();
+                    tree = frame;
+                    continue VISIT_NODE;
+                }
+                return result;
+            }
+        }
     }
 
     // post-order recursive
     static <E> List<E> postOrderRecursive(BinTree<E> tree) {
-        return new Empty<>();
+        try {
+            return postOrderRecursive(tree.getLeftT()).append(postOrderRecursive(tree.getRightT())).append(new Node<>(tree.getData(),new Empty<>()));
+        } catch (EmptyTreeE e) {
+            return new Empty<>();
+        }
     }
 
     // post-order iterative
     static <E> List<E> postOrderIterative(BinTree<E> tree) {
-	return new Empty<>();
+        List<E> result = new Empty<>();
+        Stack<BinTree<E>> stack = new Stack<>();
+        VISIT_NODE: while (true) {
+            try {
+                result = new Node<E>(tree.getData(), result);
+                stack.push(tree.getLeftT());
+                tree = tree.getRightT();
+            } catch (EmptyTreeE e) {
+                while (!stack.isEmpty()) {
+                    BinTree<E> frame = stack.pop();
+                    tree = frame;
+                    continue VISIT_NODE;
+                }
+                return result;
+            }
+        }
     }
 }
