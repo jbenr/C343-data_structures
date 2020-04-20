73dd4eb535514fdc63c628611d7bcfc6a695ee23
maazin jawad <mjawad@iu.edu>
2020-04-20 16:22:17 -0400

    Revert "hi"
    This reverts commit 614e7a5ee81f5538784846c05d4348a54be94127.

--- A10/src/WeakHeap.java
+++ A10/src/WeakHeap.java
@@ -39,39 +39,19 @@ public class WeakHeap {
      *   - for each item, a corresponding flip bit is initialized to 0
      *   - call weakHeapify
      */
-    WeakHeap(ArrayList<Item> items) {
-        this.size = items.size();
-        this.elems = new ArrayList<>();
-        this.flips = new ArrayList<>();
-
-        for (int i = 0; i < size; i++) {
-            Item old = items.get(i);
-            old.setPosition(i);
-            old.setHeap(this);
-            elems.add(old);
-            flips.add(0);
-        }
-
-        weakHeapify();
-
+    WeakHeap (ArrayList<Item> items) {
+	// TODO
     }
 
     /*
      * This method executes a loop that starts with the last element
      * in the array and ends with the element at position 1. In each
-     * iteration, the item is joined with its distinguished ancestor.
-     * Note that when calling join, the distinguished ancestor is
-     * always in the first argument position.
+     * iteration, the item is joined with its distinguished ancestor. 
+     * Note that when calling join, the distinguished ancestor is 
+     * always in the first argument position. 
      */
-    void weakHeapify() {
-
-        try {
-            for (int i = size - 1; i > 0; i--) {
-                join(getDAncestorIndex(i), i);
-            }
-        } catch (RootE e) {
-            throw new Error("Heapify: root was wrongly reached");
-        }
+    void weakHeapify () {
+	// TODO
     }
 
     // Trivial methods
@@ -92,7 +72,7 @@ public class WeakHeap {
         return elems;
     }
 
-    Item getElem(int i) {
+    Item getElem (int i) {
         return elems.get(i);
     }
 
@@ -100,7 +80,7 @@ public class WeakHeap {
         return elems.get(i).getValue();
     }
 
-    int getFlip(int i) {
+    int getFlip (int i) {
         return flips.get(i);
     }
 
@@ -111,171 +91,98 @@ public class WeakHeap {
     // Computations with indices
 
     int getParentIndex(int i) throws RootE {
-
-        if (i != 0) {
-            return i / 2;
-        }
-
-        throw new RootE();
+	// TODO
+	return 0;
     }
 
     int getLeftChildIndex(int i) throws NoLeftChildE {
-        if (i != 0 && i < size && 2 * i + getFlip(i) < size)
-            return 2 * i + getFlip(i);
-        throw new NoLeftChildE();
+	// TODO
+	return 0;
     }
 
     int getRightChildIndex(int i) throws NoRightChildE {
-
-        if (i < size && ((2 * i + 1 - getFlip(i))) < size)
-            return 2 * i + 1 - getFlip(i);
-        throw new NoRightChildE();
+	// TODO
+	return 0;
     }
 
-    boolean isLeftChild(int i) throws RootE {
-        return i != 0 && i % 2 == flips.get(getParentIndex(i));
+    boolean isLeftChild (int i) throws RootE {
+        // TODO
+	return false;
     }
 
-    boolean isRightChild(int i) throws RootE {
-        return i != 0 && i % 2 != flips.get(getParentIndex(i));
-
+    boolean isRightChild (int i) throws RootE {
+        // TODO
+	return false;
     }
 
     int getDAncestorIndex(int i) throws RootE {
-        if (isRightChild(i)) {
-            return getParentIndex(i);
-        }
-        return getDAncestorIndex(getParentIndex(i));
+	// TODO
+	return 0;
     }
 
-    int getLeftMostChildIndex() throws NoLeftChildE {
-        if (size > 1) {
-            int i = 1;
-            while (true) {
-                try {
-                    i = getLeftChildIndex(i);
-                } catch (NoLeftChildE e) {
-                    break;
-                }
-            }
-            return i;
-        }
-        throw new NoLeftChildE();
+    int getLeftMostChildIndex () throws NoLeftChildE {
+	// TODO
+	return 0;
     }
 
     // Helpers for main methods
 
     void swap(int i, int j) {
-        Item atI = elems.get(i);
-        Item atJ = elems.get(j);
-
-        atI.setPosition(j);
-        atJ.setPosition(i);
-
-        elems.set(i,atJ);
-        elems.set(j, atI);
+	// TODO -- should be identical to swap in A9
     }
 
     /*
-     * If the value at position j is smaller than the value
-     * at position i, they are swapped and the flip bit at
-     * position j is negated. In that case the method returns
+     * If the value at position j is smaller than the value 
+     * at position i, they are swapped and the flip bit at 
+     * position j is negated. In that case the method returns 
      * false. If no action was taken, the method returns true.
      */
-    boolean join(int i, int j) {
-
-        if(getValue(j) < getValue(i)){
-            swap(i,j);
-            flips.set(j,1-flips.get(j));
-            return false;
-        }
-        return true;
+    boolean join (int i, int j) {
+	// TODO
+        return false;
     }
 
     /*
-     * The method starts by joining j with its distinguished ancestor.
+     * The method starts by joining j with its distinguished ancestor. 
      * If a swap was performed, the method recursively continues by moving
      * the distinguished ancestor up. If not, the method returns immediately.
      */
-    void moveUp(int j) {
-        try {
-            int ancestor = getDAncestorIndex(j);
-            if(!join(ancestor, j)){
-                moveUp(ancestor);
-            }
-        } catch (RootE rootE) {
-
-        }
-
+    void moveUp (int j) {
+	// TODO
     }
 
     /*
      * The method starts by locating the leftmost child along the leftmost
-     * spine. It then repeatedely joins j with that leftmost child and its
-     * parents.
+     * spine. It then repeatedely joins j with that leftmost child and its 
+     * parents. 
      */
-    void moveDown(int j) {
-        try {
-            int leftMost = getLeftMostChildIndex();
-
-            while(leftMost != j){
-                join(j, leftMost);
-                leftMost = getParentIndex(leftMost);
-            }
-        } catch (NoLeftChildE | RootE e) {
-
-        }
+    void moveDown (int j) {
+	// TODO
     }
 
     // Main methods: insert and extractMin
 
     /*
-     * The method adds the new item at the end of the array making sure
-     * it calls setPosition and setHeap with the appropriate parameters
-     * and initializes the associated flip bit correctly. As a little
-     * optimization, if the inserted item is in a left child position, it
-     * will reset the flip bit of the parent to 0.
+     * The method adds the new item at the end of the array making sure 
+     * it calls setPosition and setHeap with the appropriate parameters 
+     * and initializes the associated flip bit correctly. As a little 
+     * optimization, if the inserted item is in a left child position, it 
+     * will reset the flip bit of the parent to 0. 
      * The method then calls moveUp.
      */
-    void insert(Item item) {
-
-        elems.add(item);
-        flips.add(0);
-        size++;
-        item.setHeap(this);
-        item.setPosition(size-1);
-
-        try {
-            if(isLeftChild(size-1)){
-                int parent = getParentIndex(size-1);
-                flips.set(parent, 0);
-
-            }
-            moveUp(size-1);
-        } catch (RootE rootE) {
-
-            throw new Error("Internal bug: insert. Seemingly impossible exception");
-        }
+    void insert (Item item) {
+	// TODO
     }
 
     /*
-     * Like we did in the previous and as is outlined in the lecture notes,
-     * the last item in the array is moved to location 0. And then moveDown
-     * is called. Just make sure you don't call moveDown if the array has exactly
+     * Like we did in the previous and as is outlined in the lecture notes, 
+     * the last item in the array is moved to location 0. And then moveDown 
+     * is called. Just make sure you don't call moveDown if the array has exactly 
      * one element!
      */
-    Item extractMin() {
-        // TODO
-       Item minItem = findMin();
-
-       swap(0,size-1);
-
-       elems.remove(size-1);
-       size--;
-       moveDown(0);
-
-       return minItem;
-
+    Item extractMin () {
+	// TODO
+        return null;
     }
 
     /*
@@ -283,21 +190,9 @@ public class WeakHeap {
      * of the array starting from the end until reaching index 1. For each item,
      * the method checks that it is larger than its distinguished ancestor.
      */
-    boolean checkOrder() {
-        try {
-            for (int j = size - 1; j >= 1; j--) {
-                int i = getDAncestorIndex(j);
-                if (getValue(j) < getValue(i)) return false;
-            }
-            return true;
-        }
-        catch (RootE e) {
-            throw new Error("Internal bug: checkOrder");
-        }
-
+    boolean checkOrder () {
+	// TODO
+	return false;
     }
-
-
-
 
 }
